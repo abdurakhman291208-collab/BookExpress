266a186384c3d935d89e62bd0c4fa7e228b5568b
Abdurakhman <abdurakhman291208@gmail.com>
2026-04-07 14:05:54 +0500

    обновил код

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
 });
 
 // Connect to MongoDB
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookexpress')
